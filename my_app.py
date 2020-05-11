e8d6f8c80c2a237a5229406303ac256540388518
Martin Tolmusk <martintolmusk@Martins-MacBook-Pro.local>
2020-05-11 18:52:13 +0100

    Removed some code

--- my_app.py
+++ my_app.py
@@ -1,18 +1,4 @@
 import math
-import os
-import sys
-
-import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(whotogreet):
-
-    greeting = "Hello: {}".format(whotogreet)
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
